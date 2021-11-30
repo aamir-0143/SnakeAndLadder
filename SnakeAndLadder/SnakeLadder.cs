0de6044e150e1eac5695d52e362ed6e444d3b9bb
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-11-30 19:20:21 +0530

    [Aamir] Add ; Added Snake And Ladder Program

---
+++ SnakeAndLadder/SnakeLadder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadder
+    {
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100, START = 0;
+        public int standingPositionofPlayer = 0, count = 0;
+        Random random = new Random();
+        public int DieRolling()
+        {
+            int roll = random.Next(1, 7);
+            return roll;
+        }
+        public void PlayerCheck()
+        {
+            while (standingPositionofPlayer < WINNING)
+            {
+                int dieRoll = this.DieRolling();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.standingPositionofPlayer += dieRoll;
+                        if (this.standingPositionofPlayer > WINNING)
+                        {
+                            this.standingPositionofPlayer -= dieRoll;
+                        }
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    case SNAKE:
+                        this.standingPositionofPlayer -= dieRoll;
+                        if (this.standingPositionofPlayer < START)
+                        {
+                            this.standingPositionofPlayer = START;
+                        }
+                        Console.WriteLine("Got Snake");
+                        break;
+                }
+                count++;
+                Console.WriteLine("The Player Position is :" + this.standingPositionofPlayer + " " + "with a roll of " + dieRoll);
+            }
+            Console.WriteLine("The number of times dice was rolled to win the Game is : " + count);
+            Console.WriteLine("The Player wins the Game");
+        }
+    }
+}
