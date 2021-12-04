5e4f5f9d0da010301c69729fd13cb4225a64edfd
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-04 18:49:28 +0530

    [Aamir4] Add : Added DieRolled Method

--- SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeLadder.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 namespace SnakeAndLadder
 {
-    public class SnakeAndLadder  
+    class SnakeAndLadder  
     {
         private int PlayerPosition;
+        private Random objRandom;
+        private int DieRolled;
+
+        public SnakeAndLadder()
+        {
+            objRandom = new Random();
+        }
 
         public void StartGame()
         {
             Console.WriteLine("Game Started");
             Console.WriteLine("Player Position{0}", PlayerPosition);
+            RollADie();
+        }
+        private void RollADie() 
+        {
+             DieRolled = objRandom.Next(1, 7);
+            Console.WriteLine("Dice Rolled: {0}", DieRolled);
+            PlayerPosition = PlayerPosition + DieRolled;
+            Console.WriteLine("Player Position {0}", PlayerPosition);
         }
     }
 }
