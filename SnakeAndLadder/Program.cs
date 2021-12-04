589377242dd2d42fdf557e231a116ae1342f5c74
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-04 15:43:12 +0530

    [Aamir4] Add ; Welcome To Game

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,10 +6,10 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
-            SnakeAndLadder playerOne = new SnakeAndLadder();
-            playerOne.DieRolling();
-            playerOne.PlayerCheck();
+            Console.WriteLine("=======Welcome TO SNAKE AND LADDER Game=======");
+            SnakeAndLadder SnakeAndLadder = new SnakeAndLadder();
+            SnakeAndLadder.StartGame();
+
         }
     }
 }
